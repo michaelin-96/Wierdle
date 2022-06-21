456a13f339645a71082e972cd6b1b9e5c33a1703
Michael Lin <michaelin.96@gmail.com>
2022-06-21 15:37:22 -0700

    Installed dependencies

---
+++ Wierdle-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className="App-header">
+        Weirdle
+        <h6>by Michael Lin</h6>
+      </h3>
+
+    </div>
+  );
+}
+
+export default App;
