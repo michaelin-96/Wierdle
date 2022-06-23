86f31a899c27e25f6036c9420856e37d052d5045
Michael Lin <michaelin.96@gmail.com>
2022-06-23 11:20:23 -0700

    wotd get query

--- Wierdle-app/server/controllers/wotd.js
+++ Wierdle-app/server/controllers/wotd.js
@@ -2,7 +2,26 @@ var dbconnect = require('../db');
 
 module.exports = {
   get: function (req, res) {
+    let queryStr = `SELECT word FROM wordlewb WHERE id = (SELECT wordlewb_id FROM wotd WHERE date = TO_CHAR(CURRENT_DATE, 'yyyy-mm-dd'))`;
+    dbconnect.query(queryStr)
+      .then((data) => {
+        if (data.rowCount) {
+          res.send(data.rows[0].word);
+        } else {
+          let randomWOTD = Math.floor(Math.random() * 12849);
+          let queryStr2 = `INSERT INTO wotd (date, wordlewb_id) VALUES (TO_CHAR(CURRENT_DATE, 'yyyy-mm-dd'), $1)`
+          dbconnect.query(queryStr2, [randomWOTD])
+            .then(() => {
+              dbconnect.query(queryStr)
+                .then((data) => {res.send(data.rows[0].word)})
+                .catch(err => res.send(err));
+            })
+            .catch(err => res.send(err));
+        }
 
-    console.log('Got wotd request!');
+      })
+      .catch(err => console.log(err));
   }
+
+
 }
